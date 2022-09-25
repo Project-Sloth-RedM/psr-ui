f3b97363951db3cd8bf578097317a95a43eb4b1a
MoneSuper <mome9132@gmail.com>
2022-09-25 23:21:32 +0300

    psr-ui

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game {'gta5', 'rdr3'}
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'https://github.com/Project-Sloth/psr-ui'
+
+credits 'https://github.com/sharkiller/nopixel_minigame'
+
+
+client_scripts {
+  'client/*.lua',
+}
+
+ui_page {
+  'html/index.html'
+}
+
+files {
+    'html/js/*.js',
+    'html/index.html',
+    'html/style.css',
+}
